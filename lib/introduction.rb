d6e4ae0728fef7f538dab23c0110c200f9794ef9
Garen Sahagian <g.s.sahagian@gmail.com>
2020-02-09 22:08:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,9 +4,14 @@
 # in order to complete the rest of this course and be ready for the next course!
 def introduction(name)
 return "Hi, my name is #{name}."
+end
 
 def introduction_with_language(name, language)
   return "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction_with_language_optional(name, "Ruby")
+  return "Hi my name is #{name} and I am learning to program in #{langauge}."
   
   
 
